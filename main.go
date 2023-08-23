e3a9896950106529a0a4897478eef19332324d63
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-08-23 11:01:22 +0900

    Remove CompareReader to speed up
    ```
    $ go test -bench . -benchmem
    goos: windows
    goarch: amd64
    pkg: github.com/hymkor/go-numeric-compare
    cpu: Intel(R) Core(TM) i5-6500T CPU @ 2.50GHz
    BenchmarkCompare-4       2952920               383.4 ns/op             0 B/op          0 allocs/op
    PASS
    ok      github.com/hymkor/go-numeric-compare    1.859s
    ```

--- main.go
+++ main.go
@@ -1,47 +1,47 @@
 package numeric
 
 import (
-	"io"
-	"strings"
+	"unicode/utf8"
 )
 
-func readnum(n int, sc io.RuneScanner) (int, int, error) {
+func readnum(n int, s string) (int, int, string) {
 	count := 0
-	for {
-		c, _, err := sc.ReadRune()
-		if err != nil {
-			return n, count, err
-		}
+	for len(s) > 0 {
+		c, siz := utf8.DecodeRuneInString(s)
 		if c < '0' || '9' < c {
-			sc.UnreadRune()
-			return n, count, nil
+			return n, count, s
 		}
 		n = n*10 + int(c-'0')
+		s = s[siz:]
 		count++
 	}
+	return n, count, s
 }
 
-func CompareReader(r1, r2 io.RuneScanner) int {
+func Compare(s1, s2 string) int {
 	for {
-		c1, _, err1 := r1.ReadRune()
-		c2, _, err2 := r2.ReadRune()
-
-		if err1 != nil {
-			if err2 != nil {
+		if len(s1) <= 0 {
+			if len(s1) <= 0 {
 				return 0
 			}
 			return -1
 		}
-		if err2 != nil {
+		if len(s2) <= 0 {
 			return +1
 		}
+
+		c1, siz := utf8.DecodeRuneInString(s1)
+		s1 = s1[siz:]
+
+		c2, siz := utf8.DecodeRuneInString(s2)
+		s2 = s2[siz:]
 
 		if '0' <= c1 && c1 <= '9' && '0' <= c2 && c2 <= '9' {
 			var cnt1, cnt2 int
 			n1 := int(c1 - '0')
 			n2 := int(c2 - '0')
-			n1, cnt1, err1 = readnum(n1, r1)
-			n2, cnt2, err2 = readnum(n2, r2)
+			n1, cnt1, s1 = readnum(n1, s1)
+			n2, cnt2, s2 = readnum(n2, s2)
 			if n1 < n2 {
 				return -1
 			} else if n1 > n2 {
@@ -52,18 +52,7 @@ func CompareReader(r1, r2 io.RuneScanner) int {
 			} else if cnt1 > cnt2 {
 				return -1
 			}
-			if err1 != nil {
-				if err2 != nil {
-					return 0
-				}
-				return -1
-			}
-			if err2 != nil {
-				return +1
-			}
-			continue
-		}
-		if c1 < c2 {
+		} else if c1 < c2 {
 			return -1
 		} else if c1 > c2 {
 			return +1
@@ -71,10 +60,6 @@ func CompareReader(r1, r2 io.RuneScanner) int {
 	}
 }
 
-func Compare(s1, s2 string) int {
-	return CompareReader(strings.NewReader(s1), strings.NewReader(s2))
-}
-
 func LessThan(s1, s2 string) bool {
-	return CompareReader(strings.NewReader(s1), strings.NewReader(s2)) < 0
+	return Compare(s1, s2) < 0
 }
